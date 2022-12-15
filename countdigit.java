f05c4c6c1ab29f5d9398a8ca2049ad239ae71d47
Vibhali Patil <95339053+vibhalipatil@users.noreply.github.com>
2022-12-15 05:12:09 -0800

    Add files via upload

---
+++ countdigit.java
@@ -0,0 +1,15 @@
+public class Main {
+
+  public static void main(String[] args) {
+     
+    int num:
+    Scanner sc=new Scanner(System.in)
+    int num=sc.nextInt();
+    int count = 0, 
+
+    for (num != 0; num /= 10;++count) {
+    }
+
+    System.out.println("Number of digits: " + count);
+  }
+}
