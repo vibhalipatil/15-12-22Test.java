8a7d1494c21b5e4bdfe43cb39bfd996909ff8027
Vibhali Patil <95339053+vibhalipatil@users.noreply.github.com>
2022-12-15 05:30:50 -0800

    Update countdigit.java

--- countdigit.java
+++ countdigit.java
@@ -1,13 +1,18 @@
+import java util.Scanner*;
 public class Main {
 
   public static void main(String[] args) {
-     
-    int num:
-    Scanner sc=new Scanner(System.in)
-    int num=sc.nextInt();
-    int count = 0, 
 
-    for (num != 0; num /= 10;++count) {
+    int count = 0, num ;
+    Scanner sc=new Scanner(System.in)
+     System.out.print("Enter a number"):
+    int num=sc.nextInt();
+    
+      
+
+    while (num != 0) {
+      num /= 10;
+      ++count;
     }
 
     System.out.println("Number of digits: " + count);
